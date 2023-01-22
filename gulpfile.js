94ebce5d4dcf139fa0a456a4398155aefd96a4a3
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2023-01-22 15:20:13 +0200

    add structures project

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+'use strict';
+// main module
+import gulp from 'gulp';
+// path
+import { path } from './gulp/config/path.js';
+// plugins
+import { plugins } from './gulp/config/plugins.js';
+// global
+global.app = {
+  isBuild: process.argv.includes('--build'),
+  isDev: !process.argv.includes('--build'),
+  path,
+  gulp,
+  plugins,
+};
+
+// import task
+import { copy } from './gulp/tasks/copy.js';
+import { reset } from './gulp/tasks/reset.js';
+import { html } from './gulp/tasks/html.js';
+import { server } from './gulp/tasks/server.js';
+import { scss } from './gulp/tasks/scss.js';
+import { js } from './gulp/tasks/js.js';
+import { imagesOrigin } from './gulp/tasks/imagesOrigin.js';
+import { imagesMin } from './gulp/tasks/imagesMin.js';
+import { otfToTft, ttfToWoff, fontStyle } from './gulp/tasks/fonts.js';
+import { sprite } from './gulp/tasks/sprite.js';
+import { zip } from './gulp/tasks/zip.js';
+
+// watcher files
+function watcher() {
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.scss, scss);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, imagesOrigin);
+  gulp.watch(path.watch.images, imagesMin);
+}
+
+export { sprite };
+// sequential processing fonts
+const fonts = gulp.series(otfToTft, ttfToWoff, fontStyle);
+// main task
+const mainTasksOrigin = gulp.series(
+  fonts,
+  gulp.parallel(copy, html, scss, js, imagesOrigin)
+);
+const mainTasksMin = gulp.series(
+  fonts,
+  gulp.parallel(copy, html, scss, js, imagesMin)
+);
+
+// build scenario run task
+const dev = gulp.series(reset, mainTasksOrigin, gulp.parallel(watcher, server));
+const devMin = gulp.series(reset, mainTasksMin, gulp.parallel(watcher, server));
+const build = gulp.series(reset, mainTasksOrigin);
+const deployZIP = gulp.series(reset, mainTasksOrigin, zip);
+
+export { dev };
+export { build };
+export { deployZIP };
+
+// run task
+gulp.task('default', dev);
+gulp.task('devmin', devMin);
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as flsFunctions from './files/functions.js';
+
+flsFunctions.isWebp();
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper();
