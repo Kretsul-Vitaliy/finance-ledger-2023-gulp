487b4af896ea92fe9f280235c2edc8ce5c1ecdf8
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2023-01-28 22:55:39 +0200

    fix navigator worker and add width,height img gallery

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,9 @@ import { scrollHeight } from "./files/scrollHeight.js";
 import { regexForm } from "./files/regex.js";
 
 window.addEventListener("load", async () => {
-    if ("serviceWorker" in navigator.userAgentData) {
+    if ("serviceWorker" in navigator) {
         try {
-            const reg = await navigator.userAgentData.serviceWorker.register("sw.js");
+            const reg = await navigator.serviceWorker.register("sw.js");
             console.log("Service worker register success", reg);
         } catch (e) {
             console.log("Service worker register fail");
